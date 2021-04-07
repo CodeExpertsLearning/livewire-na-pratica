d36b391d264709ba4ca764bbce431f5b939687fb
Nanderson Castro <nandokstro@gmail.com>
2021-04-07 19:59:46 -0300

    Adicionando DataTable na tela de meus registros

--- app/Http/Livewire/Expense/ExpenseList.php
+++ app/Http/Livewire/Expense/ExpenseList.php
@@ -3,17 +3,47 @@
 namespace App\Http\Livewire\Expense;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\Expense;
 
 class ExpenseList extends Component
 {
+    use WithPagination;
+
+    protected $queryString = ['search', 'orderBy', 'orderByField', 'take', 'type'];
+
+    public $search;
+    public $type;
+    public $take;
+
+    public $orderBy = 'DESC';
+    public $orderByField = 'created_at';
+
+
     public function render()
     {
-        $expenses = auth()->user()->expenses()->count() ?
-            auth()->user()->expenses()->orderBy('created_at', 'DESC')->paginate(10) :
-            [];
+        $expenses = auth()->user()->expenses()->orderBy($this->orderByField, $this->orderBy); //... ORDER By created_at DESC
+
+        $expenses->when($this->search, function($queryBuilder){
+            return $queryBuilder->where('description', 'LIKE', '%' . $this->search . '%');
+        });
+
+        $expenses->when($this->type, function($queryBuilder){
+            return $queryBuilder->where('type', $this->type);
+        });
+
+        $expenses = $this->take ? $expenses->paginate($this->take) : $expenses->get();
+
+        $expenses = auth()->user()->expenses()->count()
+            ? $expenses
+            : [];
 
         return view('livewire.expense.expense-list', compact('expenses'));
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
     }
 
     public function remove($expense)
@@ -22,5 +52,11 @@ class ExpenseList extends Component
         $exp->delete();
 
         session()->flash('message', 'Registro removido com sucesso!');
+    }
+
+    public function changeOrder($orderField, $orderBy = null)
+    {
+        $this->orderByField = $orderField;
+        $this->orderBy = $this->orderBy == 'DESC' ? 'ASC' : 'DESC';
     }
 }
--- resources/views/livewire/expense/expense-list.blade.php
+++ resources/views/livewire/expense/expense-list.blade.php
@@ -5,49 +5,129 @@
     </x-slot>
 
     <div class="w-full mx-auto text-right mb-4">
-        <a href="{{route('expenses.create')}}" class="flex-shrink-0 bg-green-700 hover:bg-green-900 border-green-700 hover:border-green-900 text-sm border-4 text-white py-2 px-6 rounded">Criar Registro</a>
+        <a href="{{route('expenses.create')}}" class="flex-shrink-0 bg-green-500 hover:bg-green-900 border-green-700 hover:border-green-900 text-sm border font-bold text-white py-2 px-6 rounded">Criar Registro</a>
     </div>
 
     @include('includes.message')
 
-    <table class="table-auto w-full mx-auto">
-        <thead>
-        <tr class="text-left">
-            <th class="px-4 py-2">#</th>
-            <th class="px-4 py-2">Descrição</th>
-            <th class="px-4 py-2">Valor</th>
-            <th class="px-4 py-2">Data Registro</th>
-            <th class="px-4 py-2">Ações</th>
-        </tr>
-        </thead>
+    <div class="flex justify-between my-5 pt-5 pb-3 border-b">
+        <div class="type">
+            <span>Carregar</span>
+            <select name="" id="" wire:model="take" class="border rounded py-2 px-4">
+                <option value="">Carregar Todos</option>
+                <option value="10">10 por página</option>
+                <option value="30">30 por página</option>
+            </select>
+        </div>
 
-        <tbody>
-        @forelse($expenses as $exp)
-            <tr>
-                <td class="px-4 py-2 border">{{$exp->id}}</td>
-                <td class="px-4 py-2 border">{{$exp->description}}</td>
-                <td class="px-4 py-2 border">
-                    <span class="{{ $exp->type == 1 ? 'text-green-600' : 'text-red-600'}}" >R$ {{number_format($exp->amount, 2, ',', '.')}}</span>
-                </td>
-                <td class="px-4 py-2 border">{{$exp->expense_date ?
-                            $exp->expense_date->format('d/m/Y H:i:s') :
-                            $exp->created_at->format('d/m/Y H:i:s')}}</td>
+        <div class="type">
+            <span>Por Tipo</span>
+            <select name="" id="" wire:model="type" class="border rounded py-2 px-4">
+                <option value="">Todos os Tipos</option>
+                <option value="1">Entrada</option>
+                <option value="2">Saída</option>
+            </select>
+        </div>
 
-                <td class="px-4 py-4 border">
-                    <a href="{{route('expenses.edit', $exp->id)}}" class="px-4 py-2 border rounded bg-teal-700 text-white">Editar</a>
-                    <a href="#" wire:click.prevent="remove({{$exp->id}})"
-                       class="px-4 py-2 border rounded bg-red-500 text-white">Remover</a>
-                </td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="5">Nenhum registro encontrado!!!</td>
-            </tr>
-        @endforelse
-        </tbody>
-    </table>
+        <div class="search">
+            <span>Buscar</span>
+            <input type="text" wire:model="search" class="border rounded py-2 px-4" placeholder="Encontre na tabela...">
+        </div>
+
+    </div>
+
+
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <button wire:click="changeOrder('id')">
+                                    #
+
+                                    @if($orderByField == 'id')
+
+                                        {!! $orderBy == 'DESC' ? '&uarr;' : '&darr;' !!}
+                                    @endif
+                                </button>
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Descrição
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <button wire:click="changeOrder('amount')">
+                                    Valor
+
+                                    @if($orderByField == 'amount')
+
+                                        {!! $orderBy == 'DESC' ? '&uarr;' : '&darr;' !!}
+                                    @endif
+                                </button>
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <button wire:click="changeOrder('created_at')">
+                                    Criado Em
+
+                                    @if($orderByField == 'created_at')
+
+                                        {!! $orderBy == 'DESC' ? '&uarr;' : '&darr;' !!}
+                                    @endif
+                                </button>
+                            </th>
+
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Ações
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+
+
+                        @foreach($expenses as $exp)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">
+                                        {{$exp->id}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">
+                                        {{$exp->description}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">
+                                        <span class="{{ $exp->type == 1 ? 'text-green-600' : 'text-red-600'}}" >R$ {{number_format($exp->amount, 2, ',', '.')}}</span>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">
+                                        {{
+                                        $exp->expense_date ?
+                                        $exp->expense_date->format('d/m/Y H:i:s') :
+                                        $exp->created_at->format('d/m/Y H:i:s')
+                                        }}
+                                    </div>
+                                </td>
+
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="{{route('expenses.edit', $exp->id)}}" class="px-4 py-2 border border-teal-900 hover:bg-teal-300 rounded bg-teal-700 text-white uppercase text-xs font-bold">Editar</a>
+                                    <a href="#" wire:click.prevent="remove({{$exp->id}})"
+                                       class="px-4 py-2 border border-red-700 rounded bg-red-500 hover:bg-red-300 text-white uppercase text-xs font-bold">Remover</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="w-full mx-auto mt-10">
-        @if(count($expenses))
+        @if($take)
             {{$expenses->links()}}
         @endif
     </div>
